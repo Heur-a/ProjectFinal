b2c77f03b3b52912c30cd57c76faf69b06f8ffb7
Saac04 <santiagoaaguirrec@gmail.com>
2023-11-24 17:51:38 +0100

    to english bruh

--- ProjectFinal/TeacherPage.aspx.cs
+++ ProjectFinal/TeacherPage.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace ProjectFinal
 {
-    public partial class PaginaProfesor : System.Web.UI.Page
+    public partial class TeacherPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
